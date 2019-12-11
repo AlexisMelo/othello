45b6096ad14e349715569ea5c26495a70c355d3d
Si Ruixu <rusi@asi-testing>
2019-12-11 08:12:22 +0100

    modification GererPartie

--- programme/src/GererPartie.c
+++ programme/src/GererPartie.c
@@ -1,7 +1,9 @@
 Couleur GererPartie(Couleur couleur,Plateau *plateau,void (*AfficherResultat)(),void (*AfficherPlateau)(Plateau),Coup (*ObtenirCoupJ1)(),void (*ObtenirCoupJ2)()){
   while(EtatDeLaPartie(couleur,*plateau)!="Terminée"){
     if (EtatDeLaPartie(couleur,*plateau)=="En Cours"){
-      plateau=JouerUnTour(plateau,ObtenirCoup,couleur);
+      plateau=JouerUnTour(plateau,ObtenirCoupJ1,couleur);
+      AfficherPlateau(*plateau);
+      plateau=JouerUnTour(plateau,ObtenirCoupJ2,couleur);
       AfficherPlateau(*plateau);
     }
     couleur=Inverser(couleur);
