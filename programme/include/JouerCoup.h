1d6ed7f226fcb6aa0bb41e46ec627c7b46dfcb55
eloudiyibenayad <el.loudiyi_ben_ayad@insa-rouen.fr>
2019-12-13 19:25:39 +0100

    refactor (jouercoup.h): modification

--- programme/include/JouerCoup.h
+++ programme/include/JouerCoup.h
@@ -1,8 +1,8 @@
+#ifndef JouerCoup_h
+#define JouerCoup_h
 #include <stdio.h>
 #include"Plateau.h"
 #include"Coup.h"
-#ifndef JouerCoup_h
-#define JouerCoup_h
 
 void JouerCoup(Plateau *plateau,Coup *coupJoue);
 
