2e0ec34fdbbca94d5a2d63cb78310bdd527fe590
eloudiyibenayad <el.loudiyi_ben_ayad@insa-rouen.fr>
2019-12-13 19:28:47 +0100

    refactor (.h): modification

--- programme/include/CalculerPoint.h
+++ programme/include/CalculerPoint.h
@@ -1,8 +1,8 @@
+#ifndef CalculerPoint_h
+#define CalculerPoint_h
 #include <stdio.h>
 #include"Couleur.h"
 #include"Plateau.h"
-#ifndef CalculerPoint_h
-#define CalculerPoint_h
 
 int CalculerPoint(Plateau *p, Couleur couleur);
 
--- programme/include/ObtenirCoupJoueur.h
+++ programme/include/ObtenirCoupJoueur.h
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include"Couleur.h"
 #ifndef ObtenirCoupJoueur_h
 #define ObtenirCoupJoueur_h
-
+#include <stdio.h>
+#include"Couleur.h"
 Coup ObtenirCoupJoueur(Couleur *c);
 
 #endif
