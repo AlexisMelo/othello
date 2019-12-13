0c9f3d275e686d09751653864b34859fc65a06af
eloudiyibenayad <el.loudiyi_ben_ayad@insa-rouen.fr>
2019-12-13 19:33:38 +0100

    refactor (calculerpoint.c): modification dans le code

--- programme/src/CalculerPoint.c
+++ programme/src/CalculerPoint.c
@@ -6,9 +6,9 @@
 
 int CalculerPoint(Plateau *p, Couleur couleur){
   int points, i, j;
-  for(i=1;i<=8;i++){
-    for(j=1;j<=8;j++){
-      if(couleur==ObtenirCouleur(ObtenirPion(plateau,position(i,j)))){
+  for(i=1;i<=ObtenirTaille(p);i++){
+    for(j=1;j<=ObtenirTaille(p);j++){
+      if(couleur==ObtenirCouleur(ObtenirPion(p,position(i,j)))){
 	    points=points+1;
       }
     }
