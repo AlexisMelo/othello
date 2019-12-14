91742a821ee976e316b73c97c374629ef1d3a330
Ruixu SI <ruixu.si@insa-rouen.fr>
2019-12-14 22:43:13 +0800

    finition GererPartie

--- programme/src/GererPartie.c
+++ programme/src/GererPartie.c
@@ -1,4 +1,4 @@
-Couleur GererPartie(Couleur couleur,Plateau *plateau,void (*AfficherResultat)(Plateau),void (*AfficherPlateau)(Plateau),Coup (*ObtenirCoupJ1)(Couleur,Plateau),void (*ObtenirCoupJ2)(Couleur,Plateau)){
+void GererPartie(Couleur couleur,Plateau *plateau,void (*AfficherResultat)(Plateau),void (*AfficherPlateau)(Plateau),Coup (*ObtenirCoupJ1)(Couleur,Plateau),void (*ObtenirCoupJ2)(Couleur,Plateau)){
   while(EtatDeLaPartie(couleur,*plateau)!="Terminée"){
     if (EtatDeLaPartie(couleur,*plateau)=="En Cours"){
       plateau=JouerUnTour(plateau,ObtenirCoupJ1,couleur);
