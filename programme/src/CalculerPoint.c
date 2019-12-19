189c163616e9de10c80b5343d280738c5486c384
eloudiyibenayad <el.loudiyi_ben_ayad@insa-rouen.fr>
2019-12-19 14:25:23 +0100

    perf(calculerpoint.c): ameliore le code

--- programme/src/CalculerPoint.c
+++ programme/src/CalculerPoint.c
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 
 int CalculerPoint(Couleur * p, Couleur couleur){
-  int points, i, j;
+  int points=0;
+  int i, j;
   for(i=1;i<=ObtenirTaille(p);i++){
     for(j=1;j<=ObtenirTaille(p);j++){
-      if(couleur==ObtenirCouleur(ObtenirCouleur(p,position(i,j)))){
+      if(couleur==obtenirCouleurDepuisPlateau(p,position(i,j)))
 	    points=points+1;
       }
     }
