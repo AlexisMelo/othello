cd769de7a3a7ce14962d87db6d0d86b560894229
Ruixu SI <ruixu.si@insa-rouen.fr>
2019-12-23 00:42:53 +0800

    doc FaireUnePartie

--- programme/include/FaireUnePartie.h
+++ programme/include/FaireUnePartie.h
@@ -2,5 +2,21 @@
 #define __FAIREUNEPARTIE__
 #include "GererPartie.h"
 #include "InitialiserPlateau.h"
+
+/**
+ *@brief Permet de faire une partie
+ *
+ * Exemple : FaireUnePartie(Couleur noir,AfficherResultat,AfficherPlateau,ObtenirCoupJ1,ObtenirCoupJ2)
+ *
+ *@param Couleur : Couleur du premier joueur qui joue
+ *
+ *@param AfficherResultat : pointeur de fonction pour afficher le resultat
+ *
+ *@param AfficherPlateau : pointeur de fonction pour afficher le plateau
+ *
+ *@param ObtenirCoupJ1 : pointeur de fonction pour obtenir le coup du premier joueur
+ *
+ *@param ObtenirCoupJ2 : pointeur de fonction pour obtenir le coup du deuxième joueur
+*/
 void FaireUnePartie(Couleur,AfficherResultat,AfficherPlateau,ObtenirCoupJ1,ObtenirCoupJ2);
 #endif
