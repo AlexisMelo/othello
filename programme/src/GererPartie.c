699cac0f7a8dd9d1c025bfbe08cae0a82e62a6fa
Ruixu SI <ruixu.si@insa-rouen.fr>
2019-12-23 00:27:20 +0800

    changer GererPartie

--- programme/src/GererPartie.c
+++ programme/src/GererPartie.c
@@ -1,14 +1,15 @@
-void GererPartie(Couleur couleur,Plateau *plateau,void (*AfficherResultat)(Plateau),void (*AfficherPlateau)(Plateau),Coup (*ObtenirCoupJ1)(Couleur,Plateau),void (*ObtenirCoupJ2)(Couleur,Plateau)){
-  while(EtatDeLaPartie(couleur,*plateau)!="Terminée"){
-    if (EtatDeLaPartie(couleur,*plateau)=="En Cours"){
-      plateau=JouerUnTour(plateau,ObtenirCoupJ1,couleur);
-      AfficherPlateau(*plateau);
-      if (EtatDeLaPartie(Inverser(couleur),*plateau)=="En Cours"){
-	plateau=JouerUnTour(plateau,ObtenirCoupJ2,Inverser(couleur));
-        AfficherPlateau(*plateau);
-      }
-    }
+void GererPartie(Couleur couleur,Couleur *plateau,void (*AfficherResultat)(Plateau),void (*AfficherPlateau)(Plateau),Coup (*ObtenirCoupJ1)(Couleur,Plateau),void (*ObtenirCoupJ2)(Couleur,Plateau)){
+  char* j1 = "En Cours";
+  char* j2 = "En Cours";
+  while((EtatDeLaPartie(couleur,plateau)!="Terminée") && (j1 != "Bloqué" || j2 != "Bloqué"){
+  j1 = EtatDeLaPartie(couleur,plateau);
+  if (j1 == "En Cours"){
+      JouerUnTour(plateau,ObtenirCoupJ1,couleur);
+      AfficherPlateau(plateau);
+   }
+  j2 = EtatDeLaPartie(Inverser(couleur),plateau);
+  if (j2 == "En Cours"){
+      JouerUnTour(plateau,ObtenirCoupJ2,Inverser(couleur));
+      AfficherPlateau(plateau);
   }
-  AfficherResultat();
 }
-    
