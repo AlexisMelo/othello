959cb12f6931bfff9178022208d57e976787b566
Ruixu SI <ruixu.si@insa-rouen.fr>
2019-12-23 00:19:18 +0800

    changement FaireUnePartie

--- programme/src/FaireUnePartie.c
+++ programme/src/FaireUnePartie.c
@@ -1,5 +1,5 @@
 void FaireUnePartie(Couleur couleur,void (*AfficherResultat)(Plateau),void (*AfficherPlateau)(Plateau),Coup (*ObtenirCoupJ1)(Couleur,Plateau),Coup (*ObtenirCoupJ2)(Couleur,Plateau)){
-  Plateau *plateau;
-  plateau=InitialiserPlateau();
+  Couleur *plateau;
+  InitialiserPlateau(plateau);
   GererPartie(couleur,plateau,AficherResultat,AfficherPlateau,ObtenirCoupJ1,ObtenirCoupJ2);
 }
