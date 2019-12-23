a35ac653e80d09dea2582a98c3b95cf1d1282453
eloudiyibenayad <el.loudiyi_ben_ayad@insa-rouen.fr>
2019-12-23 22:14:26 +0100

    docs(obtenircoupjoueur): documentation

--- programme/include/ObtenirCoupJoueur.h
+++ programme/include/ObtenirCoupJoueur.h
@@ -2,6 +2,15 @@
 #define ObtenirCoupJoueur_h
 #include <stdio.h>
 #include"Couleur.h"
+
+/**
+ *@brief Retourne un coup d'un joueur à partir d'une saisie de coordonnées et d'une couleur.
+ *
+ * @param c : Couleur
+ *
+ * @returns Coup
+*/
+
 Coup ObtenirCoupJoueur(Couleur c);
 
 #endif
