90654928f9b4d3ed3d2137937d258e0f6e4ef55c
eloudiyibenayad <el.loudiyi_ben_ayad@insa-rouen.fr>
2019-12-24 16:40:58 +0100

    perf(obtenircoupjoueur): modification

--- programme/src/ObtenirCoupJoueur.c
+++ programme/src/ObtenirCoupJoueur.c
@@ -1,14 +1,15 @@
 #include "ObtenirCoupJoueur.h"
 #include"Couleur.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 Coup ObtenirCoupJoueur(Couleur c){
   int ligneNb, colonneNb;
+printf("Entrer la valeur de la colonne");
+  scanf("%c", &colonneNb);
   printf("Entrer la valeur de la ligne");
   scanf("%d", &ligneNb);
-  printf("Entrer la valeur de la colonne");
-  scanf("%c", &colonneNb);
   Ligne ligne = obtenirLigne(ligneNb);
   Colonne colonne = obtenirColonne(colonneNb);
   return Coup(Position(ligne,colonne),c);
