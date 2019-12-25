161fdfbf8f8ba152dded53912468ba4c113a7ee9
Mesbah Zacharia <zacharia.mesbah@insa-rouen.fr>
2019-12-25 14:35:59 +0100

    Update ObtenirCoupJoueur.c

--- programme/src/ObtenirCoupJoueur.c
+++ programme/src/ObtenirCoupJoueur.c
@@ -5,14 +5,11 @@
 #include <stdlib.h>
 
 Coup ObtenirCoupJoueur(Couleur c){
-   char * entree;
+    char * entree;
     entree = (char*)malloc(5*sizeof(char));
-   char colonneLettre = entree[0]
-   int ligneNombre = entree[1]
+    char colonneLettre = entree[0]
+    int ligneNombre = entree[1]
     printf("Entrer la valeur de ton coup");
     scanf("%s", entree);
-    printf("%c", entree[0]);
-    printf("\n");
-    printf("%d", entree[1]);
-  return Coup(Position(colonneLettre, ligneNombre),c);
+  return Coup(creerPosition(colonneLettre, ligneNombre),c);
 }
