d9161a837850bb7dc1dd260b1a0e5e3ad4fcf56e
Zacharia <zacharia.mesbah@insa-rouen.fr>
2019-12-31 12:57:02 +0100

    fix(*) : Syntax + LL + Coups + bcp de petites choses

--- programme/tests/testJouerCoup.c
+++ programme/tests/testJouerCoup.c
@@ -3,41 +3,42 @@
 #include <CUnit/CUnit.h>
 #include "testJouerCoup.h"
 #include "../include/Couleur.h"
-int init_suite_success(void) { 
+/* int init_suite_success(void) { 
     return 0; 
 }
 
 int clean_suite_success(void) { 
     return 0; 
-}
+} */
 
 void TestJouerCoup(void){
-    void * plateauDeJeu = creerPlateau();
+    void * plateauDeJeu = CreerPlateau();
     Couleur NOIR = CouleurNoir();
     Couleur BLANC = CouleurBlanc();
     Couleur NEUTRE = CouleurNeutre();
     Coup coup;
-    Ligne ligne = creerLigne(1);
-    Colonne colonne = creerColonne(1);
+    Ligne ligne = CreerLigne(1);
+    Colonne colonne = CreerColonne(1);
 
    for (int i = 1; i < TAILLE + 1; i++)
     {
         for (int j = 1; j < TAILLE + 1; j++)
         {
-            coup = creerCoup(creerPosition(ligne + i - 1,colonne + j - 1), NEUTRE);
-            jouerCoup(plateauDeJeu, coup);
+            coup = CreerCoup(CreerPosition(ligne + i - 1,colonne + j - 1), NEUTRE);
+            JouerCoup(plateauDeJeu, coup);
         }
     }
 
 
-    coup = creerCoup(creerPosition(1,2), NOIR);
-    jouerCoup(plateauDeJeu, coup);
+    coup = CreerCoup(CreerPosition(1,2), NOIR);
+    JouerCoup(plateauDeJeu, coup);
     
 
 
     Couleur resultatAttendu = NOIR;
-    Couleur resultatObtenu = obtenirCouleurDepuisPlateau(plateauDeJeu, creerPosition(1,2));
-    CU_ASSERT(estEgalCouleur(resultatAttendu, resultatObtenu));
+    Couleur resultatObtenu = CouleurNeutre();
+    resultatObtenu = ObtenirCouleurDepuisPlateau(plateauDeJeu, CreerPosition(1,2));
+    CU_ASSERT(EstEgalCouleur(resultatAttendu, resultatObtenu));
 
 
 }
