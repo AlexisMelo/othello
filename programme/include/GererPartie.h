a7c317388ac0d1f54ca1539cca06a74368d75b1d
Alexis Melo da silva <alexis.melo_da_silva@insa-rouen.fr>
2019-12-31 17:42:15 +0100

    refactor(partie): fusion gererpartie.c et faireunepartie.c

--- programme/include/GererPartie.h
+++ programme/include/GererPartie.h
@@ -1,11 +1,20 @@
 #ifndef __GERERPARTIE__
 #define __GERERPARTIE__
-#include "AfficherResultat.h"
-#include "AfficherPlateau.h"
-#include "ObtenirCoupJ1.h"
-#include "ObtenirCoupJ2.h"
-#include "EtatDeLaPartie.h"
-#include "JouerUnTour.h"
+
+/**
+ * @brief Permet de lancer une partie
+ *
+ * Exemple : PARTIE_FaireUnePartie(AfficherResultat,AfficherPlateau,j1,j2)
+ * *
+ * @param AfficherResultat : pointeur de fonction pour afficher les resultats de la partie
+ *
+ * @param AfficherPlateau : pointeur de fonction pour afficher le plateau
+ *
+ * @param JoueurJ1 : Joueur qui joue en premier
+ *
+ * @param JoueurJ2 : Second joueur
+*/
+void PARTIE_FaireUnePartie(void (*AfficherResultat)(Couleur *),void (*AfficherPlateau)(Couleur *), Joueur j1, Joueur j2);
 
 /**
  *@brief permet de gerer une partie
@@ -25,4 +34,5 @@
  *@returns booléen
 */
 void GererPartie(Couleur,Couleur *,void (*AfficherResultat)(Plateau),void (*AfficherPlateau)(Plateau),Coup (*ObtenirCoupJ1)(Couleur,Plateau),void (*ObtenirCoupJ2)(Couleur,Plateau));
+
 #endif
--- programme/src/FaireUnePartie.c
+++
@@ -1,7 +0,0 @@
-#include "FaireUnePartie.h"
-
-void PARTIE_FaireUnePartie(void (*AfficherResultat)(Couleur *),void (*AfficherPlateau)(Couleur *), Joueur j1, Joueur j2){
-  Couleur* plateau = PLATEAU_CreerPlateau();
-  PLATEAU_InitialiserPlateau(plateau);
-  PARTIE_GererPartie(plateau,AficherResultat,AfficherPlateau,j1,j2);
-}
