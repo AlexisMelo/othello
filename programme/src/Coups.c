bc736078f4479d69350dd54957bdf38925792f64
Alexis Melo da silva <alexis.melo_da_silva@insa-rouen.fr>
2020-01-03 21:18:14 +0100

    add(coups): ajout fichiers Léo coups

--- programme/src/Coups.c
+++ programme/src/Coups.c
@@ -1,60 +1,38 @@
 #include "Coups.h"
+#include <stdio.h>
 
-Coups * COUPS_CreerCoups() {
-    Coups * presultat = (Coups*)malloc(sizeof(Coups)+sizeof(int));
-    presultat->coups = listeChainee();
-    presultat->nbDeCoups = 0;
-    return presultat;
+Coups COUPS_creerCoups(){
+  Coups coups;
+  coups.nbCoups = 0;
+  return coups;
 }
 
-void COUPS_AjouterCoup(Coups * coups, Coup coup) {
-    LC_ajouter(&(coups->coups), &coup, COUPCOLLECTION_FonctionCopierCoup);
-    //printf("Ajouter %d   AjouterFin", coups->coups->element->position.ligne);
-    coups->nbDeCoups += 1;
-    //printf("Ajouter %d   AjouterFin", coups->coups->element->position.colonne);
+
+bool COUPS_estVide(Coups coups){
+  return (coups.nbCoups == 0);
 }
 
-Coup COUPS_ObtenirCoup(Coups coups){
-    return *(coups.coups->element);  // *presultat;
+
+void COUPS_ajouterCoup(Coups *pCoups, Coup coup){
+    pCoups->lesCoups[pCoups->nbCoups] = coup;
+    pCoups->nbCoups++;
+
 }
 
-void COUPS_SupprimerCoupEnTete(Coups * coups){
-    Noeud noeud = *(coups->coups);
-    ListeChainee ls = noeud.listeSuivante;
-    coups->coups = ls;
-    coups->nbDeCoups -= 1;
-    COUPCOLLECTION_FonctionLibererCoup(&noeud);
+
+void COUPS_retirerCoup(Coups *pCoups){
+  Coup* tab = pCoups->lesCoups;
+  for (int i = 1; i < pCoups->nbCoups; i++) {
+    tab[i-1] = tab[i];
+  }
+  pCoups->nbCoups--;
 }
 
-int COUPS_ObtenirNombreDeCoups(Coups coups) {
-    return coups.nbDeCoups;
+unsigned int COUPS_nbCoups(Coups coups){
+  return coups.nbCoups;
 }
 
-bool COUPS_EstPresent(Coups coups, Coup coup) {
-    bool estUnCoupPresent = false;
-    while (!LC_estVide(coups.coups) && !estUnCoupPresent){
-            if (EstEgalCoup(coup, COUPS_ObtenirCoup(coups))){
-                estUnCoupPresent = true;
-            }
-            if(LC_estVide(LC_obtenirListeSuivante(coups.coups))){
-                break;
-            }
-            coups.coups = LC_obtenirListeSuivante(coups.coups);
-    }
-    return estUnCoupPresent;
-}
 
-void COUPS_RetirerCoup(Coups * coups, Coup coup) {
-    assert(EstPresent(*coups, coup));
-        Coups temp = *coups;
-        while (!LC_estVide(coups->coups)){
-            if (COUP_estEgalCoup(coup, COUPS_ObtenirCoup(*coups))){
-                ListeChainee liste = coups->coups;
-                //Noeud noeud = liste;
-                ListeChainee ls = liste->listeSuivante;
-                coups->coups = ls;
-                };
-                temp.coups = temp.coups->listeSuivante;
-
-        }
+Coup COUPS_obtenirCoup(Coups coups){
+  return coups.lesCoups[0];
 }
