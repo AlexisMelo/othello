22bcee1b137b2a9d21ddfd368577c298a0d7ca46
Alexis Melo da silva <alexis.melo_da_silva@insa-rouen.fr>
2020-01-03 19:24:45 +0100

    refactor(listechainees): suppression listes chainées

--- programme/include/elementCollection.h
+++
@@ -1,8 +0,0 @@
-#ifndef ELEMENT_COLLECTION
-#define ELEMENT_COLLECTION
-#include"Coup.h"
-#include"ListeChainee.h"
-typedef Noeud* (*EC_FonctionCopierDansCollection) (Noeud*);
-typedef void (*EC_FonctionLibererDeCollection) (Noeud*);
-typedef int (*EC_FonctionComparaison) (Noeud*,Noeud*);
-#endif
