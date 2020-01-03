0700dbaff19108870d517c1f09fcfa84e6f8c754
Alexis Melo da silva <alexis.melo_da_silva@insa-rouen.fr>
2020-01-03 22:41:40 +0100

    refactor(parcourirdirection): nouvelle verification quun coup existe dans une direction

--- programme/src/Parcourir_Direction.c
+++ programme/src/Parcourir_Direction.c
@@ -1,38 +1,52 @@
 #include"Parcourir_Direction.h"
 
-bool ParcourirLesDirections (Couleur * plateauDeJeu, Position positionDuCoup, Couleur joueurActuel) {
-    return (ParcourirUneDirection (plateauDeJeu, positionDuCoup, HG, joueurActuel)
-            || ParcourirUneDirection (plateauDeJeu, positionDuCoup, H, joueurActuel)
-            || ParcourirUneDirection (plateauDeJeu, positionDuCoup, HD, joueurActuel)
-            || ParcourirUneDirection (plateauDeJeu, positionDuCoup, D, joueurActuel)
-            || ParcourirUneDirection (plateauDeJeu, positionDuCoup, BD, joueurActuel)
-            || ParcourirUneDirection (plateauDeJeu, positionDuCoup, B, joueurActuel)
-            || ParcourirUneDirection (plateauDeJeu, positionDuCoup, BG, joueurActuel)
-            || ParcourirUneDirection (plateauDeJeu, positionDuCoup, G, joueurActuel));
+bool RECHERCHEDIRECTIONS_CoupPossibleDansUneDirectionQuelconque(Couleur * plateau, Coup coup) {
+    return (RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, HG)
+            || RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, H)
+            || RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, HD)
+            || RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, D)
+            || RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, BD)
+            || RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, B)
+            || RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, BG)
+            || RECHERCHEDIRECTIONS_CoupPossibleDansDirection(plateau, coup, G));
 }
 
+bool RECHERCHEDIRECTIONS_CoupPossibleDansDirection(Couleur * plateau, Coup coup, Direction direction) {
 
-bool ParcourirUneDirection (Couleur * plateauDeJeu, Position positionDuCoup, Direction uneDirection, Couleur joueurActuel) {
-    bool ligneEnCours, directionValide;
-    ligneEnCours = false;
-    directionValide = false;
-    AppliquerDirection(positionDuCoup, uneDirection);
-    if (EstPositionValide(positionDuCoup) && EstEgalCouleur(ObtenirCouleurDepuisPlateau(plateauDeJeu, positionDuCoup), ObtenirCouleuropposee(joueurActuel))) {
-        ligneEnCours = true;
-        while (!directionValide && ligneEnCours) {
-            AppliquerDirection (positionDuCoup, uneDirection);
-            if (EstPositionValide(positionDuCoup)) {
-                if (EstPositionVide (plateauDeJeu, positionDuCoup)) {
-                    ligneEnCours = false;
-                } else {
-                    if (EstEgalCouleur(ObtenirCouleurDepuisPlateau(plateauDeJeu, positionDuCoup), joueurActuel)) {
-                        directionValide = true;
-                    }
-                }
-            } else {
-                ligneEnCours = false;
-            }
-        }
+    Position nouvellePosition = POSITION_AppliquerDirection(COUP_ObtenirPosition(coup), direction);
+
+    if (!POSITION_EstPositionValide(plateau, nouvellePosition)) { // la position n'existe pas sur le plateau
+      return false;
     }
-    return (directionValide);
+
+    if (!COULEUR_EstEgalCouleur(PLATEAU_ObtenirCouleurAvecPosition(plateau, nouvellePosition), COULEUR_ObtenirCouleurOpposee(COUP_ObtenirCouleur(coup)))) { //la couleur de la case dans la nouvelle position n'est pas de couleur opposée à la couleur de base
+      return false;
+    }
+
+    //à partir de ce moment on sait qu'on a rencontré la couleur adverse collée au coup de départ, donc on peut potentiellement avoir un coup dans cette direction
+    bool caseVideRencontree = false;
+    bool maCouleurRencontree = false;
+    bool limitePlateauAtteinte = false;
+    while (!caseVideRencontree && !maCouleurRencontree && !limitePlateauAtteinte) {
+
+        nouvellePosition = POSITION_AppliquerDirection(nouvellePosition, direction);
+
+        limitePlateauAtteinte = !POSITION_EstPositionValide(plateau, nouvellePosition);
+
+        if (!limitePlateauAtteinte) {
+
+          if (COULEUR_EstEgalCouleur(PLATEAU_ObtenirCouleurAvecPosition(plateau, nouvellePosition), COUP_ObtenirCouleur(coup))) {
+            maCouleurRencontree = true;
+          }
+          else {
+            if (PLATEAU_EstPositionLibre(plateau, nouvellePosition)) {
+              caseVideRencontree = true;
+            }
+          }
+
+        }
+
+    }
+
+    return caseVideRencontree;
 }
