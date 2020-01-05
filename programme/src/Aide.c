0e1d31f98eeb08b1bd16d6e3a92bc57fe0fbcc77
jsaivres <jerome.saivres@insa-rouen.fr>
2020-01-05 23:50:00 +0100

    fixes encoding issues on the report

--- programme/src/Aide.c
+++ programme/src/Aide.c
@@ -4,10 +4,10 @@
 void MessageDAide () {
 printf("Aide pour le joueur : \n \n");
 printf("Deux modes de jeu sont disponibles : \n");
-printf("- Mode Standard : (blanc | noir [profondeur > 2]) \n");
+printf("- Mode Standard : (blanc | noir [profondeur > 2]) \n");
 printf("     - Permet de jouer contre l""ordinateur en lui donnant les blancs ou les noirs \n");
 printf("     - Par defaut [profondeur> = 5] \n \n");
-printf("- Mode Tournoi : (blanc | noir [profondeur > 2] \n");
+printf("- Mode Tournoi : (blanc | noir [profondeur > 2] \n");
 printf("     - Permet de faire jouer le programme dans un mode ""tournoi"" en lui donnant les blancs ou les noirs \n");
 printf("     - Par defaut [profondeur = 5] \n \n");
 printf("Comment poser un pion sur le plateau ? \n");
