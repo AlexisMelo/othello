4d250806dc897879174c81bccfa8335cc6084a86
Melo Da Silva Alexis <alexis.melo_da_silva@insa-rouen.fr>
2020-01-06 16:17:31 +0100

    Revert "Merge branch 'RedactionRapport' of..."
    This reverts commit eedf35c1c5a86e773b088b22e41e18e7d587bfbe

--- programme/src/MenuLigneCommande.c
+++ programme/src/MenuLigneCommande.c
@@ -1,6 +1,6 @@
-#include "../include/MenuLigneCommande.h"
+#include "MenuLigneCommande.h"
 
-void MenuLigneCommande(int nbArguments, char ** arguments) {
+void MENU_LC_MenuLigneCommande(int nbArguments, char ** arguments) {
 
   Joueur j1;
   Joueur j2;
@@ -15,25 +15,31 @@ void MenuLigneCommande(int nbArguments, char ** arguments) {
     j1 = JOUEUR_CreerJoueurHumain(couleurJ1);
     j2 = JOUEUR_CreerJoueurHumain(couleurJ2);
 
-    PARTIE_FaireUnePartie(AFFICHAGE_AfficherResultatsPartie, AFFICHAGE_AfficherPlateau, j1, j2);
+    printf("Jouer partie Joueur vs Joueur avec profondeur %d\n", MENU_ObtenirProfondeurIADepuisArguments(nbArguments,arguments));
+
+    PARTIE_FaireUnePartie(AFFICHAGE_AfficherResultatsPartie, AFFICHAGE_AfficherPlateau, AFFICHAGE_AfficherCoupJoue, j1, j2);
   }
   else if (strcmp(arguments[1], JOUEURVSIA) == 0) {
 
     j1 = JOUEUR_CreerJoueurHumain(couleurJ1);
     j2 = JOUEUR_CreerJoueurIA(couleurJ2, profondeurIA);
 
-    PARTIE_FaireUnePartie(AFFICHAGE_AfficherResultatsPartie, AFFICHAGE_AfficherPlateau, j1, j2);
+    printf("Jouer partie Joueur vs IA avec profondeur %d\n", MENU_ObtenirProfondeurIADepuisArguments(nbArguments,arguments)); //remplacer avec fairePartie
+
+    PARTIE_FaireUnePartie(AFFICHAGE_AfficherResultatsPartie, AFFICHAGE_AfficherPlateau, AFFICHAGE_AfficherCoupJoue, j1, j2);
 
   }
   else if (strcmp(arguments[1], IAVSIA) == 0) {
 
     j1 = JOUEUR_CreerJoueurIA(couleurJ1, profondeurIA);
-    j2 = JOUEUR_CreerJoueurIA(couleurJ2, profondeurIA);
+    j2 = JOUEUR_CreerJoueurHumain(couleurJ2);
 
-    PARTIE_FaireUnePartie(AFFICHAGE_AfficherResultatsPartieTournois, AFFICHAGE_AfficherPlateauTournois, j1, j2);
+    printf("Jouer partie IA vs IA avec profondeur %d\n", MENU_ObtenirProfondeurIADepuisArguments(nbArguments,arguments)); //remplacer avec fairePartie
+
+    PARTIE_FaireUnePartie(AFFICHAGE_AfficherResultatsPartieTournois, AFFICHAGE_AfficherPlateauTournois, AFFICHAGE_AfficherCoupJoue, j1, j2);
 
   }
   else {
-    MessageDAide();
+    AFFICHAGE_MessageAide();
   }
 }
