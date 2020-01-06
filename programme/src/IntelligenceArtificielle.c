f9e83421c66da4cff81606501eb301b470594b49
Mesbah Zacharia <zacharia.mesbah@insa-rouen.fr>
2020-01-06 22:50:49 +0100

    perf(ia)

--- programme/src/IntelligenceArtificielle.c
+++ programme/src/IntelligenceArtificielle.c
@@ -68,3 +68,73 @@ Coup IA_MinMax(Couleur * plateau, Joueur joueurAMaximiser, int Profondeur){
       return meilleurCoup;
 
 }
+int IA_alphabeta(int alpha, int beta, Couleur * plateau, int profondeurDepart, Couleur joueurActuel, Joueur JoueurAMaximiser)
+{
+  Coups coupsPossibles = RECHERCHECOUP_RechercherTousLesCoups(plateau,joueurActuel);
+  if (profondeurDepart == 0 || PLATEAU_EstRempli(plateau)){
+          return PLATEAU_CalculerPoints(plateau, joueurActuel);
+  }
+  int score =0;
+  int pts;
+    Coup BestCoup = COUPS_ObtenirCoup(coupsPossibles);
+    if (!COULEUR_SontEgalesCouleurs(JOUEUR_ObtenirCouleur(JoueurAMaximiser), joueurActuel)) { 
+          while (!COUPS_EstVide(coupsPossibles)){
+        Couleur * plateausp = PLATEAU_CreerPlateau();
+        memcpy(plateausp, plateau, TAILLE*TAILLE*sizeof(Couleur));
+        PLATEAU_JouerCoup(plateausp, COUPS_ObtenirCoup(coupsPossibles));
+        int score = -IA_alphabeta(-alpha, -beta, plateausp, profondeurDepart - 1, joueurActuel, JoueurAMaximiser); 
+        COUPS_RetirerCoup(&coupsPossibles);
+        free(plateausp);
+        if (score > alpha) {
+            alpha = score;
+            BestCoup = COUPS_ObtenirCoup(coupsPossibles);
+        COUPS_RetirerCoup(&coupsPossibles);
+          free(plateausp);
+          if (score >alpha) {
+            beta = score;
+            BestCoup = COUPS_ObtenirCoup(coupsPossibles);
+               break;
+         }
+      }
+      return alpha ;
+    }
+   }
+   else {
+        while (!COUPS_EstVide(coupsPossibles)){
+        Couleur * plateausp = PLATEAU_CreerPlateau();
+        memcpy(plateausp, plateau, TAILLE*TAILLE*sizeof(Couleur));
+        PLATEAU_JouerCoup(plateausp, COUPS_ObtenirCoup(coupsPossibles));
+        int score = IA_alphabeta(alpha, beta, plateausp, profondeurDepart - 1, joueurActuel, JoueurAMaximiser);
+        COUPS_RetirerCoup(&coupsPossibles);
+          free(plateausp);
+          if (score < beta) {
+            beta = score;
+            BestCoup = COUPS_ObtenirCoup(coupsPossibles);
+            if (alpha >= beta){
+               break;
+         }
+      }
+    }
+      return beta;
+   }
+}
+
+Coup IA_AlphaBeta(Couleur * plateau, Joueur joueurAMaximiser, int Profondeur){
+      Coups coupsPossibles = RECHERCHECOUP_RechercherTousLesCoups(plateau,JOUEUR_ObtenirCouleur(joueurAMaximiser)); // + rechercherLesCoups ?
+      Coup meilleurCoup= COUPS_ObtenirCoup(coupsPossibles);
+      int pointsMax =-1;
+      while (!COUPS_EstVide(coupsPossibles))
+      {
+        PLATEAU_JouerCoup(plateau, COUPS_ObtenirCoup(coupsPossibles));JOUEUR_ObtenirProfondeur(joueurAMaximiser);
+        int alpha = -10000;
+        int beta = 10000;
+        int resExploration = IA_alphabeta(alpha, beta,plateau,JOUEUR_ObtenirProfondeur(joueurAMaximiser), JOUEUR_ObtenirCouleur(joueurAMaximiser),joueurAMaximiser);
+        if (resExploration> pointsMax){
+          meilleurCoup = COUPS_ObtenirCoup(coupsPossibles);
+          pointsMax =resExploration;
+        }
+        COUPS_RetirerCoup(&coupsPossibles);
+      }
+      return meilleurCoup;
+
+}
